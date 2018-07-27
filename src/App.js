cb1d2f4eab1f6f8ca8b3d7689374aee4a84b31ab
Bojan Golubovic <extgcb@audatex.com>
2018-07-27 15:28:15 +0200

    Event source events

--- src/App.js
+++ src/App.js
@@ -13,9 +13,51 @@ class App extends Component {
   constructor(props) {
     super(props);
     
+    this.state = {ping: new Date(), evt: '', tickers: []};
+    
     // connect to the realtime database stream
     let API =  'https://jsdemo.envdev.io/sse';
     let eventSource = new EventSource(API);
+
+    // check if the realtime connection is dead, reload client if dead
+    setInterval(() => {
+      let now = new Date().getTime();
+      let diff = (now - this.state.ping.getTime()) / 1000;
+      
+      // haven't heard from the server in 20 secs?
+      if (diff > 20) {
+        // hard reload of client
+        window.location.reload();
+      }
+    }, 10000);
+    
+    // listen on ping from server, keep time
+    eventSource.addEventListener('ping', function(e) {
+        this.setState(previousState => {
+          return {ping: new Date(e.data)};
+        });
+    }.bind(this), false);
+    
+    // listen for database REST operations
+    eventSource.addEventListener('put', (e) => {
+        this.getTickerData()
+        
+    }, false);
+    
+    eventSource.addEventListener('delete', (e) => {
+        this.getTickerData()
+        
+    }, false);
+
+    eventSource.addEventListener('post', (e) => {
+        this.getTickerData();
+        
+    }, false);
+
+    eventSource.addEventListener("error", (e) => {
+      // typically lost network connection
+      console.log("Error", e);
+    });
 
     eventSource.onmessage = (e) => {
       this.props.dispatch(changeData(e.data));
